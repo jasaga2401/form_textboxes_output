0defe5aa887a3e61e11c9c673fb052a431836b0e
jasaga2401 <jasaga@protonmail.com>
2023-11-22 20:49:55 +0000

    Add project files.

---
+++ form_textboxes_output/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace form_textboxes_output
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(253, 111);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(177, 22);
+            this.txtEmployeeID.TabIndex = 0;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(253, 167);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(177, 22);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(253, 234);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(177, 22);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Employees";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Name";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(217, 303);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(98, 36);
+            this.btnPrevious.TabIndex = 6;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click_1);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(336, 303);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(94, 36);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Employees";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ form_textboxes_output/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace form_textboxes_output
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=Amaze\\SQLEXPRESS;Initial Catalog=stock_control;Integrated Security=True";
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;        
+
+        private SqlDataAdapter adapter;
+        private DataTable dataTable;
+        private int currentPosition = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Call a method to load data
+            LoadData();
+        }
+
+        private void btnNext_Click_1(object sender, EventArgs e)
+        {
+            if (currentPosition < dataTable.Rows.Count - 1)
+            {
+                currentPosition++;
+                DisplayData();
+            }
+            else
+            {
+                MessageBox.Show("Already at the last record.");
+            }
+        }
+
+        private void btnPrevious_Click_1(object sender, EventArgs e)
+        {
+            if (currentPosition > 0)
+            {
+                currentPosition--;
+                DisplayData();
+            }
+            else
+            {
+                MessageBox.Show("Already at the first record.");
+            }
+        }
+
+
+        private void LoadData()
+        {
+            try
+            {
+                // Initialize connection, command, and reader
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                //string query = "SELECT * FROM dbo.Employees";
+                adapter = new SqlDataAdapter("SELECT * FROM Employees", connection);
+
+                //command = new SqlCommand(query, connection);
+                //reader = command.ExecuteReader();
+
+                //dataTable = new DataTable();
+                //adapter.Fill(dataTable);
+
+                // Initialize the DataTable and fill it with data
+                dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Display data from the first record
+                DisplayData();
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void DisplayData()
+        {
+
+            if (dataTable.Rows.Count > 0)
+            {
+                DataRow row = dataTable.Rows[currentPosition];
+                // Update your form controls with data from the DataRow
+                txtEmployeeID.Text = row["EmployeeID"].ToString();
+                txtFirstName.Text = row["FirstName"].ToString();
+                txtLastName.Text = row["LastName"].ToString();
+            }
+                // row = dataTable.Rows[currentPosition];
+                // Update your form controls with data from the DataRow
+                //txtEmployeeID.Text = row["EmployeeID"].ToString();
+                //txtFirstName.Text = row["FirstName"].ToString();
+                //txtLastName.Text = row["LastName"].ToString();
+
+                // Populate TextBoxes with data from the reader
+                //txtEmployeeID.Text = reader["EmployeeID"].ToString();
+                //txtFirstName.Text = reader["FirstName"].ToString();
+                //txtLastName.Text = reader["LastName"].ToString();
+                // Add more TextBoxes for other columns as needed
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Close the reader and connection when the form is closing
+            if (reader != null && !reader.IsClosed)
+            {
+                reader.Close();
+            }
+
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        
+    }
+}
+
